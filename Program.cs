6e2f7e0900a72a207bd4d953f8f6aa481a8df5b3
Alan da Costa Santos <alancostasantos07@gmail.com>
2020-11-27 09:27:22 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Atividade_Passageiros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
